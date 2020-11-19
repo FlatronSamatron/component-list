0c8acbff5b3d4260077e97d6c2e9b135d7c13546
flatronsamatron <flatronsamatron@gmail.com>
2020-11-19 16:22:05 +0200

    fixed requests

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -2,16 +2,16 @@ import axios from 'axios'
 
 const postsApi = {
     async getPosts() {
-        const {data} = await axios.get('http://jsonplaceholder.typicode.com/posts')
+        const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts')
         return data
     },
     async addPost (post) {
-        const req = await axios.post('http://jsonplaceholder.typicode.com/posts', post)
+        const req = await axios.post('https://jsonplaceholder.typicode.com/posts', post)
         console.log(req)
         return req.data
     },
     async editPost (put,id) {
-        const req = await axios.put(`http://jsonplaceholder.typicode.com/posts/${id}`, put)
+        const req = await axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`, put)
         console.log(req)
         return req.data
     }
